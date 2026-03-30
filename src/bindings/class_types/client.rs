f980d9679a15dc49ad648630d2e5fe4a66084900
catornot <41955154+catornot@users.noreply.github.com>
2026-03-29 23:12:52 -0400

    fmt

--- src/bindings/class_types/client.rs
+++ src/bindings/class_types/client.rs
@@ -1,6 +1,6 @@
-use std::ffi::{ c_char, c_void };
+use std::ffi::{c_char, c_void};
 
-use crate::{ field_assert, size_assert };
+use crate::{field_assert, size_assert};
 
 const PERSISTENCE_MAX_SIZE: usize = 0xddcd;
 
@@ -64,14 +64,14 @@ field_assert!(M_UID where CClient, m_UID == 0xF500);
 pub enum SignonState {
     #[default]
     NONE = 0, // no state yet; about to connect
-    CHALLENGE = 1, // client challenging server; all OOB packets
-    CONNECTED = 2, // client is connected to server; netchans ready
-    NEW = 3, // just got serverinfo and string tables
-    PRESPAWN = 4, // received signon buffers
+    CHALLENGE = 1,   // client challenging server; all OOB packets
+    CONNECTED = 2,   // client is connected to server; netchans ready
+    NEW = 3,         // just got serverinfo and string tables
+    PRESPAWN = 4,    // received signon buffers
     GETTINGDATA = 5, // respawn-defined signonstate, assumedly this is for persistence
-    SPAWN = 6, // ready to receive entity packets
-    FIRSTSNAP = 7, // another respawn-defined one
-    FULL = 8, // we are fully connected; first non-delta packet received
+    SPAWN = 6,       // ready to receive entity packets
+    FIRSTSNAP = 7,   // another respawn-defined one
+    FULL = 8,        // we are fully connected; first non-delta packet received
     CHANGELEVEL = 9, // server is changing level; please wait
 }
 
